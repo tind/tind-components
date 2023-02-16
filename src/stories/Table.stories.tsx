290fba2cf5a0d2a3ad404bf7f6870b1f20c0b3be
anteAdamovic <ante.adamovic93@gmail.com>
2023-02-16 14:03:03 +0100

    Testing dynamic tables story

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Table, Column } from '../components/Table';
 import * as tableDataSmall from './demo/assets/table-data-small.json';
@@ -102,5 +102,55 @@ const PaginatorTemplate: ComponentStory<typeof Table> = (args) => {
         </Table>
     )
 };
-
 export const WithPaginator = PaginatorTemplate.bind({});
+
+interface DynamicProps { columns: number, withSelection: boolean, withActions: boolean, withPaginator: boolean }
+const DynamicTemplate: ComponentStory<typeof Table> = (args: DynamicProps | any) => {
+    const [tableData, setTableData] = useState(tableDataMedium.data);
+    const [selection, setSelection] = useState([]);
+    const tableActions = <>
+        <ButtonSet className="m-w-400">
+            <Button type="primary" label="View" icon='pi pi-eye' size="small" outlined={true}></Button>
+            <Button type="primary" label="Edit" icon='pi pi-pencil' size="small" outlined={true}></Button>
+            <Button type="primary" label="Delete" icon="pi pi-trash" size="small" outlined={true}></Button>
+            <Button type="primary" label="Unpublish" icon="pi pi-file-excel" size="small" outlined={true}></Button>
+        </ButtonSet>
+    </>;
+
+    // useEffect(() => {
+    //     setTableData(tableDataMedium.data);
+    // }, []);
+
+    const getTableColumns = (count: number = 3) => {
+        let columns = [
+            <Column field="id" header="#"></Column>,
+            <Column field="nameOfThePage" header="Name of the page"></Column>,
+            <Column field="content" header="Content"></Column>,
+            <Column field="language" header="Language"></Column>,
+            <Column field="created" header="Created" sortable={true}></Column>,
+            <Column field="modified" header="Modified" sortable={true}></Column>,
+            <Column field="status" header="Status" sortable={true}></Column>
+        ];
+
+        console.log(columns, count);
+        return columns.slice(0, count).filter((c: any) => {
+            console.log(c);
+            return c;
+        });
+    }
+
+    return (
+        <Table data={tableData} onSelectionChange={args.withSelection ? (e: any) => setSelection(e.value) : () => {}} paginator={args.withPaginator}>
+            { args.withSelection ? <Column selectionMode="multiple"></Column> : <></> }
+            { getTableColumns(args.columns) }
+            { args.withActions ? <Column body={tableActions}></Column> : <></> }
+        </Table>
+    )
+};
+export const Dynamic = DynamicTemplate.bind({});
+Dynamic.args = {
+    columns: 3,
+    withSelection: true,
+    withActions: true,
+    withPaginator: false
+}
